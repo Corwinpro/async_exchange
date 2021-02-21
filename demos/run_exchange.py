6833aa1c72bf40ebaa05b640b18046ae8c20cd78
Corwinpro <corwinat@gmail.com>
2021-02-21 08:50:50 -0800

    [DEV] InfluxDB setup and logging (#10)
    * grafana/influxdb setup
    
    * InfluxDB logger
    
    * logging transactions in the exchange
    
    * updated example script
    
    * black formatting
    
    * move grafana/influxdb setup to separate folder

--- demos/run_exchange.py
+++ demos/run_exchange.py
@@ -2,13 +2,14 @@ import asyncio
 
 from async_exchange.api import Exchange
 from async_exchange.algorithms.random_trader import RandomTrader
+from async_exchange.logging.influxdb_logger import InfluxDBLogger
 
 
 NOF_TRADERS = 100
 
 
 async def main():
-    exchange = Exchange()
+    exchange = Exchange(logger=InfluxDBLogger())
     api = exchange.api
     tasks = [
         RandomTrader(exchange_api=api, money=300, stocks=10).cycle()
