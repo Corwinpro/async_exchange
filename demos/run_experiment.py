0acbc3c13c0a347693a38dfa5779d7d85beabd0f
corwinpro <corwinat@gmail.com>
2021-03-18 22:15:27 +0000

    add demo run experiment

---
+++ demos/run_experiment.py
@@ -0,0 +1,22 @@
+from async_exchange.algorithms.random_trader import RandomTrader
+from async_exchange.experiment import Experiment
+try:
+    from async_exchange.logging.influxdb_logger import InfluxDBLogger
+except ImportError:
+    logger = None
+    RandomTrader.verbose = True
+else:
+    logger = InfluxDBLogger()
+    RandomTrader.verbose = False
+
+
+NOF_TRADERS = 100
+
+
+if __name__ == "__main__":
+    traders = [
+        RandomTrader(money=300, stocks=10)
+        for _ in range(NOF_TRADERS)
+    ]
+    experiment = Experiment(traders=traders, logger=logger)
+    experiment.run()
