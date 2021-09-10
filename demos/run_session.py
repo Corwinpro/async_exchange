46b30e8ef04524d3b5ddef599191a77412a51624
corwinpro <corwinat@gmail.com>
2021-09-10 22:44:47 +0100

    revert changes

--- demos/run_session.py
+++ demos/run_session.py
@@ -15,7 +15,6 @@ NOF_TRADERS = 100
 
 
 if __name__ == "__main__":
-
     traders = [RandomTrader(money=300, stocks=10) for _ in range(NOF_TRADERS)]
     session = TradingSession(traders=traders, logger=logger)
     session.run()
